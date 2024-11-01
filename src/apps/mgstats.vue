4981f3380959a0c3055c350ef0341cb38324ab00
Robert Laffey <rlaffey@capitaltransport.com.au>
2024-11-01 17:01:39 +1100

    Remove explicit bar component

--- src/apps/mgstats.vue
+++ src/apps/mgstats.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { get, useDropZone } from '@vueuse/core'
 import { onMounted, ref, useTemplateRef } from 'vue'
-import { Bar } from 'vue-chartjs'
 import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
 Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
